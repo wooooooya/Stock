03212b03d7d89179b802989cd0dba04b930fc2f5
YyoungsY <diddud1236@daum.net>
2025-04-13 00:24:18 +0900

    build repository

---
+++ src/main/java/com/nextstep/Stock/repository/StockPriceRepository.java
@@ -0,0 +1,10 @@
+package com.nextstep.Stock.repository;
+
+import com.nextstep.Stock.entity.StockPrice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockPriceRepository extends JpaRepository<StockPrice, Long> {
+    StockPrice findByShortCode(String shortCode);
+}
