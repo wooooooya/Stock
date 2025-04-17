b50f210c08f97b6503b25d6a08c4f7019b2bbb79
Taehyeony <teahyun0540@gmail.com>
2025-04-17 13:57:42 +0900

    add stock information crawler

---
+++ src/main/java/com/nextstep/Stock/service/crawler/StockCrawlerService.java
@@ -0,0 +1,104 @@
+package com.nextstep.stock.service.crawler;
+
+import com.nextstep.stock.entity.StockInfo;
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class StockCrawlerService {
+
+    private final WebDriver driver;
+
+    public void getStockInfo() {
+        String url = "http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020201";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        driver.get(url);
+
+        //css Selector
+        By kospiRadioSelector = By.cssSelector("#mktId_0_1");
+        By searchButtonSelector = By.cssSelector("#jsSearchButton");
+        By checkTableLoadingSelector = By.cssSelector("#jsMdiContent > div > div.CI-GRID-AREA.CI-GRID-ON-WINDOWS > div.CI-GRID-WRAPPER > div.CI-GRID-MAIN-WRAPPER > div.CI-GRID-BODY-WRAPPER > div > div > table > tbody > tr:nth-child(1) > td:nth-child(1)");
+        By stockTableSelector = By.cssSelector("#jsMdiContent > div > div.CI-GRID-AREA.CI-GRID-ON-WINDOWS > div.CI-GRID-WRAPPER > div.CI-GRID-MAIN-WRAPPER > div.CI-GRID-BODY-WRAPPER > div > div > table > tbody");
+        By loadingSelector = By.cssSelector("#jsMdiContent > div > div.CI-GRID-AREA.CI-GRID-ON-WINDOWS > div.loading-bar-wrap.small");
+
+        Set<StockInfo> stockInfoList = new HashSet<>();
+        int previousSize = 0;
+
+        try {
+            WebElement tableTemp = wait.until(driver -> {
+                try {
+                    return driver.findElement(checkTableLoadingSelector);
+                } catch (NoSuchElementException e) {
+                    return null;
+                }
+            });
+
+            WebElement kospiRadioElement = driver.findElement(kospiRadioSelector);
+            WebElement searchButtonElement = driver.findElement(searchButtonSelector);
+            js.executeScript("arguments[0].click();", kospiRadioElement);
+            js.executeScript("arguments[0].click();", searchButtonElement);
+            Thread.sleep(100);
+
+
+            wait.until(ExpectedConditions.not(ExpectedConditions.visibilityOfElementLocated(loadingSelector)));
+            Thread.sleep(100);
+
+
+            while(true){
+                WebElement tableElement = driver.findElement(stockTableSelector);
+
+                List<WebElement> rows = tableElement.findElements(By.cssSelector("tr"));
+
+                for(WebElement row: rows){
+                    String standardCode = row.findElement(By.cssSelector("td:nth-child(1)")).getText();
+                    if(standardCode.isBlank()){
+                        continue;
+                    }
+                    String shortCode = row.findElement(By.cssSelector("td:nth-child(2)")).getText();
+                    String korStockName = row.findElement(By.cssSelector("td:nth-child(3)")).getText();
+                    String korShortStockName = row.findElement(By.cssSelector("td:nth-child(4)")).getText();
+                    String engStockName = row.findElement(By.cssSelector("td:nth-child(5)")).getText();
+                    StockInfo stockInfo = StockInfo.builder()
+                            .standardCode(standardCode)
+                            .shortCode(shortCode)
+                            .korStockName(korStockName)
+                            .korShortStockName(korShortStockName)
+                            .engStockName(engStockName)
+                            .build();
+                    System.out.println(stockInfo.toString());
+                    stockInfoList.add(stockInfo);
+                }
+
+                js.executeScript("""
+                                        let scroller = document.querySelector('#jsMdiContent > div > div.CI-GRID-AREA.CI-GRID-ON-WINDOWS > div.CI-GRID-WRAPPER > div.CI-FREEZE-SCROLLER');
+                                        scroller.scrollTop += 500;
+                                        scroller.dispatchEvent(new Event('scroll'));""");
+                Thread.sleep(500);
+
+                if (stockInfoList.size() == previousSize) break;
+                previousSize = stockInfoList.size();
+
+                System.out.println(stockInfoList.size());
+            }
+            System.out.println(stockInfoList.size());
+
+        } catch (TimeoutException e) {
+            System.out.println("타임아웃: 요소를 찾지 못했습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("예외 발생");
+        }
+    }
+
+}
